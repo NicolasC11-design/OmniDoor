2077e4a0fb5541522b02c3b028fdec4a99a51737
cristianobeltranjosephsebastia-design <cristianobeltranjosephsebastia@gmail.com>
2026-05-29 22:01:04 -0500

    dev_estructuraFrontend

---
+++ Frontend/src/app/componentes/dashboard-administrador/dashboard-administrador.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-administrador',
+  imports: [],
+  templateUrl: './dashboard-administrador.html',
+  styleUrl: './dashboard-administrador.css',
+})
+export class DashboardAdministrador {}
