7ea6c12e29b2d16c58b0ac2c081200bd4c368846
NicolasC11-design <jostynnicolascristianobeltran@gmail.com>
2026-06-12 18:26:32 -0500

    chore: guardar cambios locales antes de actualizar

--- Backend/OmniApi/OmniApi/settings.py
+++ Backend/OmniApi/OmniApi/settings.py
@@ -72,12 +72,18 @@ WSGI_APPLICATION = 'OmniApi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'OmniDoor',          
+        'USER': 'postgres',            
+        'PASSWORD': 'S3b4syN1c01101',    
+        'HOST': 'db.lfhheiqhptjijodxaedm.supabase.co',          
+        'PORT': '5432',                 
     }
-}
+    }
+
 
 
 # Password validation
